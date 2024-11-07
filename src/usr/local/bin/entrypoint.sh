fbd1c5514345eb5183371bb408235178a3b66c56
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2024-11-07 08:28:30 +0000

    ✨ Host based tunnel (#22)
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- src/usr/local/bin/entrypoint.sh
+++ src/usr/local/bin/entrypoint.sh
@@ -6,8 +6,11 @@ set -e
 export ERROR_LOG_LEVEL="${ERROR_LOG_LEVEL:-error}"
 export PROXY_LISTEN_ADDRESS="${PROXY_LISTEN_ADDRESS:-"*"}"
 export PROXY_LISTEN_PORT="${PROXY_LISTEN_PORT:-3000}"
+export PROXY_TUNNEL_LISTEN_ADDRESS="${PROXY_TUNNEL_LISTEN_ADDRESS:-"*"}"
+export PROXY_TUNNEL_LISTEN_PORT="${PROXY_TUNNEL_LISTEN_PORT:-3001}"
 export UPSTREAM_HOST="${UPSTREAM_HOST:-localhost}"
 export UPSTREAM_PORT="${UPSTREAM_PORT:-8080}"
+export UPSTREAM_TUNNEL_PORT="${UPSTREAM_TUNNEL_PORT:-8081}"
 
 # Auth0
 export ANALYTICAL_PLATFORM_TOOL="${ANALYTICAL_PLATFORM_TOOL:-vscode}"
@@ -18,9 +21,11 @@ export LOGOUT_URL="${LOGOUT_URL:-"https://google.com"}"
 export REDIRECT_DOMAIN="${REDIRECT_DOMAIN:-"http://localhost:3000"}"
 export USERNAME="${USERNAME:-analyticalplatform}"
 
-echo "Error log level: ${ERROR_LOG_LEVEL}"
-echo "Proxy address: ${PROXY_LISTEN_ADDRESS}:${PROXY_LISTEN_PORT}"
-echo "Proxy Upstream: ${UPSTREAM_HOST}:${UPSTREAM_PORT}"
+echo "Error log level:       ${ERROR_LOG_LEVEL}"
+echo "IDE proxy address:     ${PROXY_LISTEN_ADDRESS}:${PROXY_LISTEN_PORT}"
+echo "IDE proxy upstream:    ${UPSTREAM_HOST}:${UPSTREAM_PORT}"
+echo "Tunnel proxy address:  ${PROXY_TUNNEL_LISTEN_ADDRESS}:${PROXY_TUNNEL_LISTEN_PORT}"
+echo "Tunnel proxy upstream: ${UPSTREAM_HOST}:${UPSTREAM_TUNNEL_PORT}"
 
 echo "Createing NGINX configuration from template"
 cp /etc/nginx/nginx.conf.template /etc/nginx/nginx.conf
@@ -29,8 +34,11 @@ echo "Replacing NGINX settings placeholders in NGINX configuration"
 sed -i "s/ERROR_LOG_LEVEL/${ERROR_LOG_LEVEL}/g" /etc/nginx/nginx.conf
 sed -i "s/PROXY_LISTEN_ADDRESS/${PROXY_LISTEN_ADDRESS}/g" /etc/nginx/nginx.conf
 sed -i "s/PROXY_LISTEN_PORT/${PROXY_LISTEN_PORT}/g" /etc/nginx/nginx.conf
+sed -i "s/PROXY_TUNNEL_LISTEN_ADDRESS/${PROXY_TUNNEL_LISTEN_ADDRESS}/g" /etc/nginx/nginx.conf
+sed -i "s/PROXY_TUNNEL_LISTEN_PORT/${PROXY_TUNNEL_LISTEN_PORT}/g" /etc/nginx/nginx.conf
 sed -i "s/UPSTREAM_HOST/${UPSTREAM_HOST}/g" /etc/nginx/nginx.conf
 sed -i "s/UPSTREAM_PORT/${UPSTREAM_PORT}/g" /etc/nginx/nginx.conf
+sed -i "s/UPSTREAM_TUNNEL_PORT/${UPSTREAM_TUNNEL_PORT}/g" /etc/nginx/nginx.conf
 
 echo "Replacing Auth0 settings placeholders in NGINX configuration"
 sed -i "s/ANALYTICAL_PLATFORM_TOOL/${ANALYTICAL_PLATFORM_TOOL}/g" /etc/nginx/nginx.conf
